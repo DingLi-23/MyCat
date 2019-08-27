9004a3d9b13f4f233c25f0179d93640b4b63753d
胡仁杰 <52514277+RJie-47@users.noreply.github.com>
2019-08-27 08:32:51 +0800

    猫的预设件（实现了刚体以及按键控制跳跃）
    实现了刚体以及按键控制跳跃

---
+++ CatGame/Assets/Scripts/hurenjie/cat_move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat_move : MonoBehaviour
+{
+    private Rigidbody2D rig = null;
+    public float Force = 75.0f;
+    public float Speed = 3.0f;
+    private bool JetActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JetActive = Input.GetButton("Fire1");
+    }
+
+    private void FixedUpdate()
+    {
+        if (JetActive)
+        {
+            rig.AddForce(new Vector2(0, Force));
+        }
+        rig.velocity = new Vector2(Speed, rig.velocity.y);
+    }
+}
