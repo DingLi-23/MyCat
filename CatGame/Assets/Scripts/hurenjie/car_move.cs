a88b321cd6866696c75719de62f5838b501e8dee
丁力 <52514268+DingLi-23@users.noreply.github.com>
2019-08-29 10:59:30 +0800

    补充组合预制体
    单个预制体的组合

---
+++ CatGame/Assets/Scripts/hurenjie/car_move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_move : MonoBehaviour
+{
+    private Rigidbody2D car = null;
+    public float Force = 75.0f;
+
+    [Tooltip("用于设置小车水平速度")]
+    public float Speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        car.velocity = new Vector2(Speed, car.velocity.y);
+    }
+}
