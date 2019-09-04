29c3e671d0532e00b0df6b7f2ae4a2fa4afa5e50
胡仁杰 <52514277+RJie-47@users.noreply.github.com>
2019-09-04 16:34:44 +0800

    小车

---
+++ CatGame/Assets/Scripts/hurenjie/car_move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_move : MonoBehaviour
+{
+    private Rigidbody2D car = null;
+
+
+    [Tooltip("用于设置小车水平速度")]
+    public float Speed = 3.0f;
+
+    public bool catCloseCar = false;
+
+    void Start()
+    {
+        car = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        if (catCloseCar == true)
+        {
+            car.velocity = new Vector2(Speed, car.velocity.y);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("cat"))
+        {
+            catCloseCar = true;
+        }
+        if (GameObject.FindGameObjectWithTag("inverter"))
+        {
+
+            Speed = -Speed;
+        }
+        if (collision.gameObject.CompareTag("track_edge1") && GameObject.FindGameObjectWithTag("track_edge2"))
+        {
+            transform.GetComponent<Collider>().enabled = false;
+        }
+    }
+    
+    }
