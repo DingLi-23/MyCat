fdd31080c2eb123ee81fe44859ab8c4cb98bf1f9
胡仁杰 <52514277+RJie-47@users.noreply.github.com>
2019-09-07 11:37:58 +0800

    按键波（无法生成后自动销毁）

---
+++ CatGame/Assets/Scripts/hurenjie/camera_controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour
+{
+    public GameObject Keyingwave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(KW, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            createKW();
+        }
+    }
+    private void createKW()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Vector3 mp = Input.mousePosition;
+            Vector3 keyW = Camera.main.ScreenToWorldPoint(mp);
+            Vector3 newW = new Vector3(keyW.x, keyW.y, 0);
+            GameObject KW = Instantiate(Keyingwave, newW, Quaternion.identity);
+        }
+    }
+}
