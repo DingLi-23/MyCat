32db0188366c071c7d147ed9aa25a43cc0af72f3
李炳宽 <52514328+chinaLbk@users.noreply.github.com>
2019-09-08 16:53:51 +0800

    1
    1

---
+++ CatGame/Assets/Scripts/dingli/CameraMove1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove1 : MonoBehaviour
+{
+    public GameObject Keyingwave;
+    private Transform m_Transform;
+    private Transform cat_Transform;
+
+    public float StartTime;
+    public float EndTime;
+    private float time;
+
+    void Start()
+    {
+        m_Transform = gameObject.GetComponent<Transform>();
+        cat_Transform = GameObject.FindGameObjectWithTag("cat").GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        m_Transform.position = cat_Transform.position + new Vector3(2,0,-10);
+    }
+}
